d095363c662103f8f501458e431674e8d7d2a661
Ahmet Cilingir <face4forev@icloud.com>
2026-09-22 17:30:45 +0200

    Build My Habits screen

---
+++ HabitPing/HabitTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  HabitTableViewCell.swift
+//  HabitPing
+//
+//  Created by Ahmet CILINGIR on 22.09.26.
+//
+
+import UIKit
+
+final class HabitTableViewCell: UITableViewCell {
+    
+    @IBOutlet  weak var emojiLabel: UILabel!
+    @IBOutlet  weak var nameLabel: UILabel!
+    @IBOutlet  weak var detailLabel: UILabel!
+    @IBOutlet  weak var notificationImageView: UIImageView!
+    
+    
+}
--- HabitPing/MyHabitsViewController.swift
+++ HabitPing/MyHabitsViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class MyHabitsViewController: UIViewController {
 
+    @IBOutlet private weak var tableView: UITableView!
+    @IBOutlet private weak var emptyStateView: UIView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -22,10 +25,42 @@ class MyHabitsViewController: UIViewController {
             target: self,
             action: #selector(addButtonTapped)
         )
+
+        tableView.dataSource = self
+
+        emptyStateView.isHidden = true
     }
 
     @objc func addButtonTapped() {
         print("Add habit tapped")
     }
 
+}
+
+extension MyHabitsViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int)
+        -> Int
+    {
+        return 3
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath)
+        -> UITableViewCell
+    {
+        guard
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: "HabitCell",
+                for: indexPath
+            ) as? HabitTableViewCell
+        else { return UITableViewCell() }
+
+        cell.emojiLabel.text = "🐶"
+        cell.nameLabel.text = "Feed the dog"
+        cell.detailLabel.text = "Every morning"
+        cell.notificationImageView.image = UIImage(systemName: "bell.fill")
+
+        return cell
+
+    }
 }
